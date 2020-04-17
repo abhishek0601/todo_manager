4c31481b426016d7521bc66ea907e7794d102632
abhishek0601 <abhishek.nithya@gmail.com>
2020-04-17 20:51:11 +0530

    Todo Manager displays todos of current user

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,14 +1,14 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    current_user
+    @todo = Todo.of_user(current_user)
     render "index"
   end
 
   def show
     id = params[:id]
-    todo = Todo.find(id)
-    render plain: todo.to_pleasant_string
+    todo = Todo.of_user(current_user).find(id)
+    render "todo"
   end
 
   def create
@@ -18,6 +18,7 @@ class TodosController < ApplicationController
       todo_text: todo_text,
       due_date: due_date,
       completed: false,
+      user_id: current_user.id,
     )
     redirect_to todos_path
   end
@@ -25,7 +26,7 @@ class TodosController < ApplicationController
   def update
     id = params[:id]
     completed = params[:completed]
-    todo = Todo.find(id)
+    todo = Todo.of_user(current_user).find(id)
     todo.completed = completed
     todo.save!
     redirect_to todos_path
@@ -33,7 +34,7 @@ class TodosController < ApplicationController
 
   def destroy
     id = params[:id]
-    todo = Todo.find(id)
+    todo = Todo.of_user(current_user).find(id)
     todo.destroy
     redirect_to todos_path
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :ensure_user_logged_in
 
   def index
     render plain: User.order(:id).map { |user| user.to_pleasant_string }.join("\n")
@@ -12,13 +12,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(
+    user = User.create!(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
-    redirect_to "/"
+    session[:current_user_id] = user.id
+    redirect_to todos_path
   end
 
   def validate
